0b259fe40d06a98c755b19dc5a6d0389e59f4824
Abhinav Jadhav <101034007+Adikad16@users.noreply.github.com>
2024-11-28 22:31:48 +0530

    Add files via upload

---
+++ E-commerce/src/app/global.variables.ts
@@ -0,0 +1,8 @@
+// This file contains variables available to all components in Angular
+
+"use strict";
+
+// export const hostedAPI = "https://backend-shopkart.herokuapp.com"
+export const hostedAPI = "https://shopkart-backend-9lhx.onrender.com"
+export const localAPI = "http://localhost:3300/"
+
